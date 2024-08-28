423e60fba55e75a85926d407d1487f39d3292dd7
Clemens Horn <clemens.horn@mni.thm.de>
2024-08-28 09:17:12 +0200

    chore: update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,7 +7,7 @@ Import-Module PSReadLine
 Set-PSReadLineOption -EditMode Vi
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 
-function yy {
+function yazicd {
     $tmp = [System.IO.Path]::GetTempFileName()
     yazi $args --cwd-file="$tmp"
     $cwd = Get-Content -Path $tmp
@@ -17,12 +17,32 @@ function yy {
     Remove-Item -Path $tmp
 }
 
+function rr {
+    yazicd $env:USERPROFILE\.local\src
+}
+
+function ne {
+    yazicd $env:MOON
+}
+
 Set-PSReadLineKeyHandler -Key "Ctrl+o" -ScriptBlock {
     [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("yy")
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("yazicd")
     [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
 }
 
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 Import-Module -Name Terminal-Icons
+Import-Module git-aliases -DisableNameChecking
+
+Import-Module -Name "C:\Users\clay\moon.ps1"
+Import-Module -Name "C:\Users\clay\proto.ps1"
+
+# proto
+$env:PROTO_HOME = Join-Path $HOME ".proto";
+$env:PATH = @(
+  (Join-Path $env:PROTO_HOME "shims")
+  (Join-Path $env:PROTO_HOME "bin")
+  $env:PATH
+) -join [IO.PATH]::PathSeparator;
